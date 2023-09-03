e1c85df3f0775f959bde9fd76a72af30201bb71e
philip dutra <monsterphilip10@gmail.com>
2023-09-03 13:00:48 -0400

    completed coode along

--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,13 @@ from department import Department
 
 import ipdb
 
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)  # <Department 2: Human Resources, Building C, East Wing>
+
 ipdb.set_trace()
